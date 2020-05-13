0cb774e913cd479c7926b0d5b2db53f85f6eec65
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2020-05-13 17:58:03 -0300

    update polimorfism and heranca

--- cursoprog/src/entites/BusinessAccount.java
+++ cursoprog/src/entites/BusinessAccount.java
@@ -23,7 +23,7 @@ public class BusinessAccount extends AccountHeranca{
 	
 	public void loan(double amount) {
 		if (amount <= loanLimit) {
-			deposit(amount);
+			balance += amount - 10.0;
 		}
 
 	}
