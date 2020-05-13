d7b395137b900b5cb7fc9fd5c246c9f02ddc3f1d
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2020-05-13 17:55:16 -0300

    coding examples using polimorfism and heranca

---
+++ cursoprog/src/entites/BusinessAccount.java
@@ -0,0 +1,32 @@
+package entites;
+
+public class BusinessAccount extends AccountHeranca{
+
+	private Double loanLimit;
+	
+	public  BusinessAccount() {
+		super();//using the logic of constructor's the super class
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			deposit(amount);
+		}
+
+	}
+
+	
+}
