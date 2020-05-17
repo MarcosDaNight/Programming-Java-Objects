cf029833d50fe21fab0e5fa72e3074c77dc62720
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2020-05-17 00:16:56 -0300

    other exercise resolution

---
+++ cursoprog/src/exercicios/ImportedProduct.java
@@ -0,0 +1,34 @@
+package exercicios;
+
+public class ImportedProduct extends Product{
+	
+	private Double customsFee;
+
+	
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice() {
+		return super.getPrice() + customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ "(Custom fee : $ " 
+				+ String.format("%.2f", customsFee) 
+				+ ")";
+	}
+	
+	
+}
